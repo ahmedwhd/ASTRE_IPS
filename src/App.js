9a1da468aa7ced80ff377def23f889b3139a77a9
ahmedwhd <ahmed37300@hotmail.fr>
2021-10-15 22:58:36 +0200

    update

--- src/App.js
+++ src/App.js
@@ -14,26 +14,26 @@ function App() {
 
 
   const [kTechno, setkTechno] = useState(-3);
-  const [kLinux, setkLinux] = useState(-5);
+  const [kLinux, setkLinux] = useState(-4);
   const [kEnsimElec, setkEnsimElec] = useState(-5);
   const [kMontage, setkMontage] = useState(-3);
   const [kPlacement, setkPlacement] = useState(-3);
   const [kDiriger, setkDiriger] = useState(-2);
-  const [kPartirDeBase, setkPartirDeBase] = useState(-2);
+  const [kPartirDeZero, setkPartirDeZero] = useState(-2);
 
-  const [kEcrans, setkEcrans] = useState(4);
+  const [kEcrans, setkEcrans] = useState(3);
   const [kNetflix, setkNetflix] = useState(3);
-  const [kEquipe, setkEquipe] = useState(4);
+  const [kEquipe, setkEquipe] = useState(3);
   const [kColocation, setkColocation] = useState(3);
-  const [kArtistique, setkArtistique] = useState(4);
+  const [kArtistique, setkArtistique] = useState(5);
   const [kCreatif, setkCreatif] = useState(5);
-  const [kSoirees, setkSoirees] = useState(3);
+  const [kSoirees, setkSoirees] = useState(2);
 
-  const [kEnsimElec_MontagePC_Linux, setkEnsimElec_MontagePC_Linux] = useState(-8);
-  const [kNetflix_PlusieursEcrans, setkNetflix_PlusieursEcrans] = useState(5);
+  const [kEnsimElec_MontagePC_Linux, setkEnsimElec_MontagePC_Linux] = useState(-7);
+  const [kNetflix_PlusieursEcrans_Artistique, setkNetflix_PlusieursEcrans_Artistique] = useState(6);
   const [kLinux_Actualite_Devant_, setkLinux_Actualite_Devant_] = useState(-5);
-  const [kCreatif_Artistique_Soirees_Equipe_Mac, setkCreatif_Artistique_Soirees_Equipe_Mac] = useState(7);
-  const [kColocation_PartirDeBase, setkColocation_PartirDeBase] = useState(3);
+  const [kCreatif_Artistique_Soirees_Equipe_Mac, setkCreatif_Artistique_Soirees_Equipe_Mac] = useState(6);
+  const [kColocation_PartirDeZero, setkColocation_PartirDeZero] = useState(4);
   const [kDiriger_MontagePC, setkDiriger_MontagePC] = useState(-2);
 
   var donneesAtre = [];
@@ -78,10 +78,10 @@ function App() {
           var BDE;
           var BDLC;
           var EnsimElec_MontagePC_Linux;
-          var Netflix_PlusieursEcrans;
+          var Netflix_PlusieursEcrans_Artistique;
           var Linux_Actualite_Devant;
           var Creatif_Artistique_Soirees_Equipe_Mac;
-          var Colocation_PartirDeBase;
+          var Colocation_PartirDeZero;
           var Diriger_MontagePC;
 
           //Recuperation de las liste des assos de l'etudiant
@@ -162,35 +162,35 @@ function App() {
 
           // Hypotheses
           EnsimElec_MontagePC_Linux = kEnsimElec * EnsimElec + kMontage * montagePc + kLinux * linux;
-          Netflix_PlusieursEcrans = kNetflix * netflix + kEcrans * Ecrans;
-          console.log(Netflix_PlusieursEcrans);
+          Netflix_PlusieursEcrans_Artistique = kNetflix * netflix + kEcrans * Ecrans +  kArtistique * parseInt(data[key].artistique)/4  ;
+          console.log(Netflix_PlusieursEcrans_Artistique);
 
-          Linux_Actualite_Devant = kPlacement * placement + kLinux * linux;
-          Creatif_Artistique_Soirees_Equipe_Mac = kCreatif * parseInt(data[key].creatif)/4 /*+ kArtistique * parseInt(data[key].artistique)*/ + kSoirees * parseInt(data[key].soirees)/4 + kEquipe * parseInt(data[key].equipe)/4 /*+ kLinux * linux*/;
+          Linux_Actualite_Devant = kPlacement * placement + kLinux * linux + kTechno * parseInt(data[key].techno)/4;
+          Creatif_Artistique_Soirees_Equipe_Mac = kCreatif * parseInt(data[key].creatif)/4 + kSoirees * parseInt(data[key].soirees)/4 + kEquipe * parseInt(data[key].equipe)/4;
 
-          Colocation_PartirDeBase = kColocation * Colocation + kPartirDeBase * PartirDuneBase;
+          Colocation_PartirDeZero = kColocation * Colocation + kPartirDeZero * PartirDuneBase;
           Diriger_MontagePC = kDiriger * Diriger + kMontage * montagePc;
          
           // Calcul score
-          score[key] = ( (kEnsimElec_MontagePC_Linux * EnsimElec_MontagePC_Linux) + (kTechno * parseInt(data[key].techno)/4) + (kNetflix * netflix) + (kLinux * linux) + (kMontage * montagePc) + (kEquipe * parseInt(data[key].equipe)/4) + (kArtistique * parseInt(data[key].artistique)/4) + (kCreatif * parseInt(data[key].creatif)/4) + kPartirDeBase * PartirDuneBase + kColocation*Colocation + (kSoirees* parseInt(data[key].soirees)/4) +kDiriger*Diriger
-            + (kNetflix_PlusieursEcrans * Netflix_PlusieursEcrans) + (kLinux_Actualite_Devant_ * Linux_Actualite_Devant) + (kCreatif_Artistique_Soirees_Equipe_Mac * Creatif_Artistique_Soirees_Equipe_Mac))+ (kColocation_PartirDeBase * Colocation_PartirDeBase) + (kDiriger_MontagePC*Diriger_MontagePC)/10;  
+          score[key] = (   /*(kTechno * parseInt(data[key].techno)/4) + (kNetflix * netflix) + (kLinux * linux) + (kMontage * montagePc) + (kEquipe * parseInt(data[key].equipe)/4) + (kArtistique * parseInt(data[key].artistique)/4) + (kCreatif * parseInt(data[key].creatif)/4) + kPartirDeZero * PartirDuneBase + kColocation*Colocation + (kSoirees* parseInt(data[key].soirees)/4) +kDiriger*Diriger +kPlacement*placement + kEcrans*Ecrans + kEnsimElec*EnsimElec*/
+             (kNetflix_PlusieursEcrans_Artistique * Netflix_PlusieursEcrans_Artistique) + (kEnsimElec_MontagePC_Linux * EnsimElec_MontagePC_Linux) + (kLinux_Actualite_Devant_ * Linux_Actualite_Devant) + (kCreatif_Artistique_Soirees_Equipe_Mac * Creatif_Artistique_Soirees_Equipe_Mac)+ (kColocation_PartirDeZero * Colocation_PartirDeZero) + (kDiriger_MontagePC*Diriger_MontagePC))/10;  
 
           //Insertion des etudiants dans l'une des trois categories
-          if ((score[key]) < 0) {
+          if ((score[key]) < -0.25) {
 
             donneesAtre[key] = [data[key].numEtu, score[key]];
             listEtudiantAstre.push(data[key].numEtu);
             donneesIps[key] = [data[key].numEtu, null];
             donneesNeutre[key] = [data[key].numEtu, null];
 
-          } if ((score[key]) == 0) {
+          } if ((score[key]) <= 0.25  && (score[key]) >= -0.25) {
 
             donneesNeutre[key] = [data[key].numEtu, score[key]];
             listEtudiantNeutre.push(data[key].numEtu);
             donneesAtre[key] = [data[key].numEtu, null];
             donneesIps[key] = [data[key].numEtu, null];
 
-          } if ((score[key]) > 0) {
+          } if ((score[key]) > 0.25) {
 
             donneesIps[key] = [data[key].numEtu, score[key]];
             listEtudiantIps.push(data[key].numEtu);
@@ -215,7 +215,7 @@ function App() {
 
       })
   },
-    [kNetflix, kTechno, kLinux, kEquipe, kCreatif, kArtistique, kMontage, kEnsimElec_MontagePC_Linux, kLinux_Actualite_Devant_, kNetflix_PlusieursEcrans, kCreatif_Artistique_Soirees_Equipe_Mac,kPlacement,kEcrans, kEnsimElec, kEcrans,kColocation])
+    [kNetflix, kTechno, kLinux, kEquipe, kCreatif, kArtistique, kMontage, kEnsimElec_MontagePC_Linux, kLinux_Actualite_Devant_, kNetflix_PlusieursEcrans_Artistique, kCreatif_Artistique_Soirees_Equipe_Mac,kPlacement,kEcrans, kEnsimElec, kEcrans,kColocation,kColocation_PartirDeZero,kDiriger_MontagePC])
 
   const options = {
     chart: {
@@ -254,7 +254,7 @@ function App() {
       {
         name: 'Neutre ' + listeNumNeutre.length,
         data: dataNeutre,
-        color: "4dffc3"
+        color: "#4dffc3"
       }],
   };
 
@@ -312,7 +312,9 @@ function App() {
     chart: {
       plotBackgroundColor: null,
       plotBorderWidth: 0,
-      plotShadow: false
+      plotShadow: false,
+      height: 300,
+      width: 300,
     },
     title: {
       text: 'Prediction <br>choix des <br> options',
@@ -362,76 +364,82 @@ function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+    <h1>Astre-IPS</h1>
       <div className="range" >
         <div className="inputRange" >
           <label style={{ width: "5em" }}>Netflix</label>
-          <input type="range" min="-5" max="10" value={kNetflix} onChange={(e) => { setkNetflix(e.target.value) }} />
+          <input type="range" min="-5" max="5" value={kNetflix} onChange={(e) => { setkNetflix(e.target.value) }} />
           <label style={{ width: "5em", marginLeft: "3em" }}>{kNetflix}</label>
         </div>
 
         <div className="inputRange" >
-          <label style={{ width: "5em" }}>Techno</label>
-          <input type="range" min="-5" max="10" value={kTechno} onChange={(e) => { setkTechno(e.target.value) }} />
+          <label style={{ width: "5em" }}>Technologie</label>
+          <input type="range" min="-5" max="5" value={kTechno} onChange={(e) => { setkTechno(e.target.value) }} />
           <label style={{ width: "5em", marginLeft: "3em" }}>{kTechno}</label>
         </div>
 
 
         <div className="inputRange" >
           <label style={{ width: "5em" }}>Linux</label>
-          <input type="range" min="-5" max="10" value={kLinux} onChange={(e) => { setkLinux(e.target.value) }} />
+          <input type="range" min="-5" max="5" value={kLinux} onChange={(e) => { setkLinux(e.target.value) }} />
           <label style={{ width: "5em", marginLeft: "3em" }}>{kLinux}</label>
         </div>
 
 
         <div className="inputRange" >
           <label style={{ width: "5em" }}>EnsimElec</label>
-          <input type="range" min="-5" max="10" value={kEnsimElec} onChange={(e) => { setkEnsimElec(e.target.value) }} />
+          <input type="range" min="-5" max="5" value={kEnsimElec} onChange={(e) => { setkEnsimElec(e.target.value) }} />
           <label style={{ width: "5em", marginLeft: "3em" }}>{kEnsimElec}</label>
         </div>
         <div className="inputRange" >
           <label style={{ width: "5em" }}>Ecrans</label>
-          <input type="range" min="-5" max="10" value={kEcrans} onChange={(e) => { setkEcrans(e.target.value) }} />
+          <input type="range" min="-5" max="5" value={kEcrans} onChange={(e) => { setkEcrans(e.target.value) }} />
           <label style={{ width: "5em", marginLeft: "3em" }}>{kEcrans}</label>
         </div>
 
         <div className="inputRange" >
           <label style={{ width: "5em" }}>Montage</label>
-          <input type="range" min="-5" max="10" value={kMontage} onChange={(e) => { setkMontage(e.target.value) }} />
+          <input type="range" min="-5" max="5" value={kMontage} onChange={(e) => { setkMontage(e.target.value) }} />
           <label style={{ width: "5em", marginLeft: "3em" }}>{kMontage}</label>
         </div>
 
 
         <div className="inputRange" >
           <label style={{ width: "5em" }}>Artistique</label>
-          <input type="range" min="-5" max="10" value={kArtistique} onChange={(e) => { setkArtistique(e.target.value) }} />
+          <input type="range" min="-5" max="5" value={kArtistique} onChange={(e) => { setkArtistique(e.target.value) }} />
           <label style={{ width: "5em", marginLeft: "3em" }}>{kArtistique}</label>
         </div>
 
 
         <div className="inputRange" >
           <label style={{ width: "5em" }}>Creatif</label>
-          <input type="range" min="-5" max="10" value={kCreatif} onChange={(e) => { setkCreatif(e.target.value) }} />
+          <input type="range" min="-5" max="5" value={kCreatif} onChange={(e) => { setkCreatif(e.target.value) }} />
           <label style={{ width: "5em", marginLeft: "3em" }}>{kCreatif}</label>
         </div>
 
 
         <div className="inputRange" >
           <label style={{ width: "5em" }}>Equipe</label>
-          <input type="range" min="-5" max="10" value={kEquipe} onChange={(e) => { setkEquipe(e.target.value) }} />
+          <input type="range" min="-5" max="5" value={kEquipe} onChange={(e) => { setkEquipe(e.target.value) }} />
           <label style={{ width: "5em", marginLeft: "3em" }}>{kEquipe}</label>
         </div>
 
         <div className="inputRange" >
           <label style={{ width: "5em" }}>Colocation</label>
-          <input type="range" min="-5" max="10" value={kColocation} onChange={(e) => { setkColocation(e.target.value) }} />
+          <input type="range" min="-5" max="5" value={kColocation} onChange={(e) => { setkColocation(e.target.value) }} />
           <label style={{ width: "5em", marginLeft: "3em" }}>{kColocation}</label>
         </div>
 
         <div className="inputRange" >
           <label style={{ width: "5em" }}>Placement</label>
-          <input type="range" min="-5" max="10" value={kPlacement} onChange={(e) => { setkPlacement(e.target.value) }} />
+          <input type="range" min="-5" max="5" value={kPlacement} onChange={(e) => { setkPlacement(e.target.value) }} />
           <label style={{ width: "5em", marginLeft: "3em" }}>{kPlacement}</label>
+        </div>
+
+        <div className="inputRange" >
+          <label style={{ width: "5em" }}>Soirees</label>
+          <input type="range" min="-5" max="5" value={kSoirees} onChange={(e) => { setkSoirees(e.target.value) }} />
+          <label style={{ width: "5em", marginLeft: "3em" }}>{kSoirees}</label>
         </div>
 
       </div>
@@ -445,26 +453,32 @@ function App() {
 
         <div className="inputRange1" >
           <label style={{ width: "15em" }}>Creatif, soirees et equipe</label>
-          <input type="range" min="-5" max="10" value={kCreatif_Artistique_Soirees_Equipe_Mac} onChange={(e) => { setkCreatif_Artistique_Soirees_Equipe_Mac(e.target.value) }} />
+          <input type="range" min="-10" max="10" value={kCreatif_Artistique_Soirees_Equipe_Mac} onChange={(e) => { setkCreatif_Artistique_Soirees_Equipe_Mac(e.target.value) }} />
           <label style={{ width: "5em", marginLeft: "5em" }}>{kCreatif_Artistique_Soirees_Equipe_Mac}</label>
         </div>
 
         <div className="inputRange1" >
-          <label style={{ width: "15em" }}> Netflix et plus d'un ecran + </label>
-          <input type="range" min="-5" max="10" value={kNetflix_PlusieursEcrans} onChange={(e) => { setkNetflix_PlusieursEcrans(e.target.value) }} />
-          <label style={{ width: "5em", marginLeft: "5em" }}>{kNetflix_PlusieursEcrans}</label>
+          <label style={{ width: "15em" }}> Netflix, plus d'un ecran et artistique </label>
+          <input type="range" min="-10" max="10" value={kNetflix_PlusieursEcrans_Artistique} onChange={(e) => { setkNetflix_PlusieursEcrans_Artistique(e.target.value) }} />
+          <label style={{ width: "5em", marginLeft: "5em" }}>{kNetflix_PlusieursEcrans_Artistique}</label>
         </div>
 
         <div className="inputRange1" >
-          <label style={{ width: "15em" }}> Linux, actualite et devant</label>
+          <label style={{ width: "15em" }}> Linux, technologie et devant</label>
           <input type="range" min="-10" max="10" value={kLinux_Actualite_Devant_} onChange={(e) => { setkLinux_Actualite_Devant_(e.target.value) }} />
           <label style={{ width: "5em", marginLeft: "5em" }}>{kLinux_Actualite_Devant_}</label>
         </div>
 
         <div className="inputRange1" >
-          <label style={{ width: "15em" }}>Colocation et PartirDeBase</label>
-          <input type="range" min="-5" max="10" value={kColocation_PartirDeBase} onChange={(e) => { setkColocation_PartirDeBase(e.target.value) }} />
-          <label style={{ width: "5em", marginLeft: "5em" }}>{kColocation_PartirDeBase}</label>
+          <label style={{ width: "15em" }}>Colocation et PartirDeZero</label>
+          <input type="range" min="-10" max="10" value={kColocation_PartirDeZero} onChange={(e) => { setkColocation_PartirDeZero(e.target.value) }} />
+          <label style={{ width: "5em", marginLeft: "5em" }}>{kColocation_PartirDeZero}</label>
+        </div>
+
+        <div className="inputRange1" >
+          <label style={{ width: "15em" }}>Diriger et MontagePC</label>
+          <input type="range" min="-10" max="10" value={kDiriger_MontagePC} onChange={(e) => { setkDiriger_MontagePC(e.target.value) }} />
+          <label style={{ width: "5em", marginLeft: "5em" }}>{kDiriger_MontagePC}</label>
         </div>
       </div>
       <figure className="graphes">
